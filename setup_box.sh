fadec998d7f3da0fc6cae4d50908cd82cd240c30
abhishek kona <sheki@abhishesMacBook.lan>
2015-11-04 18:41:02 -0800

    beef up setup script

--- setup_box.sh
+++ setup_box.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -e
+brew install vim git ag
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 ln -sf $(pwd)/.vimrc "$HOME/.vimrc"
 vim -i NONE -c VundleClean -c quitall
 vim -i NONE -c VundleUpdate -c quitall
